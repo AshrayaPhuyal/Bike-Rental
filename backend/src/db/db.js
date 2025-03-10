40980f230373755c38533cd2fc7585b1f896ff8b
AshrayaPhuyal <np03cs4a220234@heraldcollege.edu.np>
2025-03-10 21:42:42 +0545

    login,singup,admin profile,admin dashboard,user profile, forget password,inventory

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,16 +1,21 @@
 import mongoose from "mongoose";
 
-
-export async function connectDb(){
-    try {
-        const connectionInstance=await mongoose.connect(process.env.MONGO_URI)
-        if(!connectionInstance){
-            console.log("Error in connecting to the database")}
-        else{
-            console.log("Connected to the database",connectionInstance.connection.host)
-        }
-
-    } catch (error) {
-        console.log("Error in connecting to the database",error.message)
+export async function connectDb() {
+  try {
+    if (!process.env.DATABASE_URI || !process.env.DATABASE_NAME) {
+      throw new Error(
+        "DATABASE_URI or DATABASE_NAME is not defined in environment variables."
+      );
     }
+
+    const connectionString = `${process.env.DATABASE_URI}/${process.env.DATABASE_NAME}`;
+    const connectionInstance = await mongoose.connect(connectionString);
+
+    console.log(
+      "mongodb database connected , host name:",
+      connectionInstance.connection.host
+    );
+  } catch (error) {
+    console.log("database connection failed in db.js file", error);
+  }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,42 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import { connectDb } from "./db/db.js"
+import express, { urlencoded } from "express";
+import "dotenv/config";
+import cors from "cors";
+import { connectDb } from "./db/db.js";
+import { rootRouter } from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
+const app = express();
 
-const app = express()
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(
+  urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
 
+app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 
-connectDb().then(() => {
-    app.listen(3000, () => {
-        console.log(`Server is running on port 3000`)
-    })
-})   
+app.use("/api/v1", rootRouter);
 
-
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`app is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongoDb connection error", error);
+  });
